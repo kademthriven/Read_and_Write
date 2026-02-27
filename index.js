ee129dabae9fd25d025b3d727c08d9eebf6645f3
Thriven Komarr <thrivenkomarr@gmail.com>
2026-02-26 23:13:11 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
+    if(req.url === '/'){
+        res.setHeader('Content-Type', 'text/html');
+        res.end(`
+            <form action="/message" method="POST">
+            <label>Name:</label>
+            <input type="text" name="Username">
+            <button type="submit">Add</button>
+            </form>
+        `);
+    }else if(url === '/message'){
+        res.setHeader('Content-Type', 'text/html');
+        let datachunks = [];
+
+        req.on('data', (chunk) => {
+            datachunks.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedData = Buffer.concat(datachunks).toString();
+            console.log(parsedData);
+            const message = parsedData.split('=')[1];
+            fs.writeFile('formvalues.txt',message, (err) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        });    
+    }else{
+        if(req.url ==='/read'){
+            fs.readFile('formvalues.txt', (err, data) => {
+                console.log(data.toString());
+                res.end(`<h1>${data.toString()}</h1>`);
+            });
+        }
+    }
+});
+server.listen(3000, () => {
+    console.log('Server is running');
+})
